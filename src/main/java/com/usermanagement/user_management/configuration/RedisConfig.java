d8e626745796ed06d86a8caddf55eb1c84361c80
muzammilnm <zammils61@gmail.com>
2025-06-07 20:37:21 +0700

    add configuration redis

---
+++ src/main/java/com/usermanagement/user_management/configuration/RedisConfig.java
@@ -0,0 +1,23 @@
+package com.usermanagement.user_management.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@Configuration
+public class RedisConfig {
+    
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory(){
+        return new LettuceConnectionFactory("localhost", 6379);
+    }
+
+    @Bean
+    public RedisTemplate<String, String> redisTemplate() {
+        RedisTemplate<String, String> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory());
+        return template;
+    }
+}
